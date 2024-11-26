b5c941b7cf9abbe7fc6ec7ffa809004205fda0d1
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-11-27 03:13:31 +0530

    Update widgets.dart

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -45,3 +45,121 @@ const CustomCard({
     );
   }
 }
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({required this.text, required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 330.0,
+      height: 55.0,
+
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          backgroundColor: Color.fromARGB(255, 252, 163, 17),
+          
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Color.fromARGB(255, 0, 0, 0),
+            fontSize: 22.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class CustomTextInput extends StatelessWidget {
+  final String hintText;
+  final TextEditingController? controller;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final ValueChanged<String>? onChanged;
+  final double width;
+  final double height;
+
+  const CustomTextInput({
+    Key? key,
+    required this.hintText,
+    this.controller,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.onChanged,
+    this.width = 272,
+    this.height = 60,
+  }) : super(key: key);
+
+
+  static const textInputDecoration = InputDecoration(
+    hintText: "Enter text",
+    hintStyle: TextStyle(color: Colors.black, fontSize: 18),
+    fillColor: Color.fromARGB(255, 255, 255, 255),
+    filled: true,
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Color.fromARGB(255, 217, 217, 217)),
+      borderRadius: BorderRadius.all(
+        Radius.circular(8),
+      ),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Color.fromARGB(255, 252, 163, 17)),
+      borderRadius: BorderRadius.all(
+        Radius.circular(8),
+      ),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      onChanged: onChanged,
+      decoration: textInputDecoration.copyWith(
+        hintText: hintText,
+      ),
+    );
+  }
+}
+
+
+class SButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const SButton({required this.text, required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      // width: 250.0,
+      // height: 50.0,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          backgroundColor: Colors.transparent,
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Color.fromARGB(255, 220, 12, 12),
+            fontSize: 15.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
