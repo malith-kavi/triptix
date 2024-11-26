8eb539830ccfa6d797adeb5f365c478426bdc9ad
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-11-27 02:22:17 +0530

    Update landing_screen.dart

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:triptix/constants.dart';
+import 'package:triptix/widgets/widgets.dart';
+
+var logo = 'assets/images/logo.png';
 
 class LandingScreen extends StatelessWidget {
   const LandingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          SafeArea(
+            child: Container(  
+              color: Colors.white,
+                child: Center(
+                  child:Padding(
+                    padding: const EdgeInsets.all(1),
+                    child: Image.asset(
+                      logo,
+                      width: 300,
+                      height: 300,
+                        ),
+                      ),
+                  ),
+                ),
+              ),
+  
+          Expanded(
+            child:Container(
+              decoration: const BoxDecoration(
+                color: bColor,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(32),
+                    topRight: Radius.circular(32),
+                  ),
+              ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 40, 20, 25),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Simplify Your\n Travel Plans',
+                        style: HeaderText2,
+                      ),
+                       Spacer(),
+                      SizedBox(
+                        height: 300,
+                        child: GridView.count(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 15,
+                        mainAxisSpacing: 15,
+                        childAspectRatio: 1,
+                        children: [
+                          CustomCard(icon: Icons.bus_alert_rounded, text: 'Driver Login', onTap: (){}),
+                          CustomCard(icon: Icons.emoji_people_rounded, text: 'User Login', onTap: (){}),
+                          
+                        ]
+                      ),
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
