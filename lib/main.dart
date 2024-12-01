9f1e15ac7db9670f04164d15129c04ab5ee18f4f
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-12-02 01:38:28 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:triptix/screens/booking_details_screen.dart';
 import 'package:triptix/screens/bus_search_screen.dart';
 import 'package:triptix/screens/driver_login_screen.dart';
 import 'package:triptix/screens/driver_register_screen.dart';
 import 'package:triptix/screens/landing_screen.dart';
 import 'package:triptix/screens/passenger_login_screen.dart';
 import 'package:triptix/screens/passenger_register_screen.dart';
+import 'package:triptix/screens/route_results_screen.dart';
+import 'package:triptix/screens/your_booking_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BusSearchScreen(),
+      home: YourBookingScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
