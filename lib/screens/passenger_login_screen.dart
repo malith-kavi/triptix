7c380291df1379b724aec9e850d813b7cc06f2ab
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-12-29 23:13:28 +0530

    Update passenger_login_screen.dart

--- lib/screens/passenger_login_screen.dart
+++ lib/screens/passenger_login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:triptix/constants.dart';
+import 'package:triptix/screens/passenger_register_screen.dart';
 import 'package:triptix/widgets/widgets.dart';
 
 var logo = 'assets/images/logo.png';
@@ -40,52 +41,59 @@ class PassengerLoginScreen extends StatelessWidget {
               ),
                 child: Padding(
                   padding: const EdgeInsets.fromLTRB(20, 40, 20, 25),
-                  child: Column(
-                    
-                    children: [
-                      Spacer(),
-                      Text(
-                        ' WELCOME',
-                        style: HeaderText2,
-                      ),
-                      SizedBox(height: 20,),
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.white,
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(10)),
-                          child: Padding(padding: EdgeInsets.all(15),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Email',style: BodyText1,),
-                              SizedBox(height: 5,),
-                              CustomTextInput(hintText: "Enter your Email"),
-                              SizedBox(height: 25,),
-                              Text('Password',style: BodyText1,),
-                              SizedBox(height: 5,),
-                              CustomTextInput(hintText: 'Enter your Password'),
-                              SizedBox(height: 25,),
-                              CustomButton(text: 'Sign In', onPressed: (){}),
-                              SizedBox(height: 10,),
-                              SButton(text: 'Forgot password?', onPressed: (){})
-                            ],
-                          ),
-                          ),
-                      ),
-                      SizedBox(height: 15,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Don’t have an account ?',style: BodyText2,),
-                          SButton(text: 'Sign Up', onPressed: (){})
-                        ],
-                      ),
-                      Spacer(),
-                  ],
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        
+                        Text(
+                          ' WELCOME',
+                          style: HeaderText2,
+                        ),
+                        SizedBox(height: 20,),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10)),
+                            child: Padding(padding: EdgeInsets.all(15),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Email',style: BodyText1,),
+                                SizedBox(height: 5,),
+                                CustomTextInput(hintText: "Enter your Email"),
+                                SizedBox(height: 25,),
+                                Text('Password',style: BodyText1,),
+                                SizedBox(height: 5,),
+                                CustomTextInput(hintText: 'Enter your Password'),
+                                SizedBox(height: 25,),
+                                CustomButton(text: 'Sign In', onPressed: (){}),
+                                SizedBox(height: 10,),
+                                SButton(text: 'Forgot password?', onPressed: (){})
+                              ],
+                            ),
+                            ),
+                        ),
+                        SizedBox(height: 15,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Don’t have an account ?',style: BodyText2,),
+                            SButton(
+                              text: 'Sign Up', 
+                              onPressed: (){
+                                Navigator.pushReplacement(context,
+                                MaterialPageRoute(builder: (context) => PassengerRegisterScreen()));
+                              })
+                          ],
+                        ),
+                        
+                    ],
+                  ),
                 ),
               ),
             ),
